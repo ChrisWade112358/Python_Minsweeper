d94e5d7b38614d24a168132858591437a4fac6b8
swade781977 <swade781977@yahoo.com>
2022-04-25 18:49:01 -0400

    Added thinter into main, setup window, top frame, fleft side frame, created settings.py and utilities.py(empty)

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+from tkinter import *
+import settings
 
-def projectName:
-    print("Python Minesweeper project")
+root = Tk()
+# Override the settings of the window
+root.configure(
+    bg='#f39a03'
+)
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+root.resizable(FALSE, FALSE)
+root.title("Python_Minesweeper")
+
+top_frame = Frame(
+    root,
+    bg= 'red', #Change later
+    width= 1440,
+    height= 180
+)
+
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+    root,
+    bg= 'blue', # Change color later
+    width=360,
+    height=540
+)
+
+left_frame.place(x=0, y=180)
+
+# Run the window
+root.mainloop()
 
 
