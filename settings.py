358f2b4a4574130f5c48144788a0499a88b63c7d
swade781977 <swade781977@yahoo.com>
2022-04-25 20:45:48 -0400

    fixed error in color palette dictionary

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 WIDTH= 1440
 HEIGHT= 720
 
-bg = {'orange': '#f39a03', 'green': '#B49D00', 'brown': '#514538', 'blue':, '#98C0C5', 'red': '#BE5846'}
+bg = {'orange': '#f39a03', 'green': '#B49D00', 'brown': '#514538', 'blue': '#98C0C5', 'red': '#BE5846'}
